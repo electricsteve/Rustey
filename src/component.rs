aa9b3c2d92ef8625d9e31aefd8b69894e6f0a134
electricsteve <96793824+electricsteve@users.noreply.github.com>
2026-02-23 00:25:08 +0100

    add issue URL

--- src/component.rs
+++ src/component.rs
@@ -1,7 +1,8 @@
 use poise::Command;
 use serenity::all::EventHandler;
 
-// TODO: standardized permission checks 
+// TODO: standardized permission checks
+// Issue URL: https://github.com/electricsteve/RustDiscordBot/issues/1
 // Implement permission checks for commands so modules don't have to check them manually.
 // Could maybe contain some logic so modules can specify what kind of permission checks to use.
 pub struct Component {
