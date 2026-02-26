71c8ac7086b103979abe583714980402e2928768
electricsteve <96793824+electricsteve@users.noreply.github.com>
2026-02-26 22:42:04 +0100

    chore: code clean-up

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod component;
 
 use poise::{serenity_prelude as serenity, PrefixFrameworkOptions};
 use std::env;
-use std::sync::Arc;
 
 struct Handler;
 
@@ -26,17 +25,6 @@ struct Data {} // User data, which is stored and accessible in all command invoc
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-/// Displays your or another user's account creation date
-// #[poise::command(slash_command, prefix_command)]
-// async fn age(
-//     ctx: Context<'_>,
-//     #[description = "Selected user"] user: Option<serenity::User>,
-// ) -> Result<(), Error> {
-//     let u = user.as_ref().unwrap_or_else(|| ctx.author());
-//     let response = format!("{}'s account was created at {}", u.name, u.created_at());
-//     ctx.say(response).await?;
-//     Ok(())
-// }
 
 #[tokio::main]
 async fn main() {
